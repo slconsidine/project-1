53370fdb5d87155721cf035f49209ea5d723b0f3
Wayne Wu <mrwu42@gmail.com>
2020-02-11 21:22:48 -0500

    added pulling json data. couldnt get the photo work. work in progress

--- assets/api test.js.js
+++ assets/api test.js.js
@@ -1,17 +1,27 @@
 $(document).on("click" ,function(){
+    $("#searches").empty();
     // var type = $(this).data("type");
     var queryURL = "https://api.petfinder.com/v2/animals/?api_key=9DDrWGvQUnYJpd0VlIUQWniNSHKnGDKBFCPikX6momuA9oYlKi";
     $.ajax({
         url:queryURL,
         method:"GET", 
         headers:{
-            "Authorization":"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjZmYzNhZDM3MWRlMTA2MjY2YmRjYzg0Mjk5NWM3NjIwOTk4MTJlNTFmODdlNThjOWIxYTJjOGE4Y2Q2YzhiZDNmMTNmNmMxYjMzODBmNWU4In0.eyJhdWQiOiI5RERyV0d2UVVuWUpwZDBWbElVUVduaU5TSEtuR0RLQkZDUGlrWDZtb211QTlvWWxLaSIsImp0aSI6IjZmYzNhZDM3MWRlMTA2MjY2YmRjYzg0Mjk5NWM3NjIwOTk4MTJlNTFmODdlNThjOWIxYTJjOGE4Y2Q2YzhiZDNmMTNmNmMxYjMzODBmNWU4IiwiaWF0IjoxNTgxNDY1MTU5LCJuYmYiOjE1ODE0NjUxNTksImV4cCI6MTU4MTQ2ODc1OSwic3ViIjoiIiwic2NvcGVzIjpbXX0.Td6ZXJLIDPcOF6z_QboieVjsF2mGGN-O7z2fH8JQoUhYWmsb8edO0Ge6SiUw4fFn1eSkwzH7Rnl0Iq4ZeXY3Iao96JFMKl4LCRFiaiQ5VuqkWItkXvvichT-N_VvZHSeSSIC2DGLyIEuESiV66wPzEpVC-Ed-n1E-XQymNtAkoYehtg4TkEaq7B8j28ZgIPKvehn4cHjlHkVhV553IBAfj_cco5VJOm3l7DzwbLBtU-9pRkEKmZiUFsVB1hSrpeB-TteDYn0qRU93OzR8oiBJqeXFKpcdiBdo-h7oPVTLbmydGOEtVzqsHdGRdsMnH3d07XOR3s3rrcx_9Hyr0aOPA" 
+            "Authorization":"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImM5NDY3NDgyYmNhNzkyM2E4ZmRlMTQyOTdhNDk3Y2Q4ZWRlZGY3MzljYmFhNmRkNDMwOThhODMzOWU2OTQ4MjI2NzIyM2FiN2U1ZTZmOTE3In0.eyJhdWQiOiI5RERyV0d2UVVuWUpwZDBWbElVUVduaU5TSEtuR0RLQkZDUGlrWDZtb211QTlvWWxLaSIsImp0aSI6ImM5NDY3NDgyYmNhNzkyM2E4ZmRlMTQyOTdhNDk3Y2Q4ZWRlZGY3MzljYmFhNmRkNDMwOThhODMzOWU2OTQ4MjI2NzIyM2FiN2U1ZTZmOTE3IiwiaWF0IjoxNTgxNDcxMDMxLCJuYmYiOjE1ODE0NzEwMzEsImV4cCI6MTU4MTQ3NDYzMSwic3ViIjoiIiwic2NvcGVzIjpbXX0.UoJ5aF-WbOzZCA4QfIODTLQXl68kbFg91_kM38pLG_ZLTtuVd6QOxggcGR_LkDyuXUOfhs-zHOL6GAJ5BecVtSMGjg1EFwC3tKUbe9vPjFLWm3m-2F-zd6glmTfwYWa1JYV-Q2XWuEt5a7wfssYLJ1tyVQQB0IJ-r1CakDYFRZvof81qaFIdXnRYepX1-jmiCDO6QTW-oXDt3tDubalQitvE_YDtwz34jVzHpT-Z2jDsRnuAu_Dm9Nh-dV9DGdjggYmo41CYeN-l3cJ3Vof1tOBV54T1qTYYC5y5_pzwyqZKE75ZcmAcjihp-9EtiIW3YmT71SgkOWp8drqWOmvoWQ" 
         }
-})
 
-    .done(function(response){
-        console.log(response)
-
+        
+    })
+        .done(function(response){
+         console.log(response)
+         for(var i=0; i<response.animals.length; i++){
+                var searchDiv = $("<div>");
+                var photo = response.animals[i].url;
+                console.log(response.animals[i].url);
+                // var image = $("<img>");
+                // image.attr("src", photo);
+                searchDiv.append(photo);
+                $("#searches").append(searchDiv);
+    }
     })
 
 })
