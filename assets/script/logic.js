6aa9d7fbed6ea38dc826c5e18d69ec2faf912a68
Matthew Globosits <matthewglobosits@gmail.com>
2020-02-15 11:39:46 -0500

    add hide button

--- assets/script/logic.js
+++ assets/script/logic.js
@@ -23,3 +23,32 @@ $("#submit").click(function() {
     return false;
 });
 
+
+
+
+
+console.log("connected");
+// hides both form show/hidden buttons
+$("#show").hide();
+$("#hide").hide();
+
+// show button shows the form
+$("#show").click(function() {
+    console.log("show");
+    $(".container").show();
+});
+
+// hide button hides the form
+$("#hide").click(function() {
+    console.log("hide");
+    $(".container").hide();
+});
+
+// submit button hides the form and displays the hide/show buttons
+$("#submit").click(function() {
+    $(".container").hide();
+    $("#show").show();
+    $("#hide").show();
+    return false;
+});
+
